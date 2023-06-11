e23eaac8a256488e7dd6abe79a273bf8f245ca34
Tal Akerman <akermania@gmail.com>
2023-06-11 17:02:09 +0200

    Adding fans control

--- RaspberryPi/reset.sh
+++ RaspberryPi/reset.sh
@@ -3,7 +3,11 @@
 echo "Resetting Process";
 ps -ef | grep arduino.py | grep -v grep | awk '{print $2}' | xargs kill ;
 sleep 3;
+ps -ef | grep fans.py | grep -v grep | awk '{print $2}' | xargs kill ;
+sleep 3;
 cd /home/pi/AllSkyX/RaspberryPi/ ;
 /home/pi/AllSkyX/RaspberryPi/arduino.py &> /home/pi/AllSkyX/RaspberryPi/output.log &
+/home/pi/AllSkyX/RaspberryPi/fans.py &> /home/pi/AllSkyX/RaspberryPi/fans.log &
 echo "Done";
 exit 0;
+
