9cab4199777c77e20909ad3b200868b782e2ddc4
Tal Akerman <akermania@gmail.com>
2023-06-11 15:46:51 +0200

    Update reset.sh

--- RaspberryPi/reset.sh
+++ RaspberryPi/reset.sh
@@ -3,6 +3,7 @@
 echo "Resetting Process";
 ps -ef | grep arduino.py | grep -v grep | awk '{print $2}' | xargs kill ;
 sleep 3;
+cd /home/pi/AllSkyX/RaspberryPi/ ;
 /home/pi/AllSkyX/RaspberryPi/arduino.py &> /home/pi/AllSkyX/RaspberryPi/output.log &
 echo "Done";
 exit 0;
